e1e99da63ce84683cc7bb65aa38f9fb9ef08b424
wizmed <47180081+wizmed@users.noreply.github.com>
2024-09-09 20:44:09 +0200

    Update RecipeController.php

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -16,7 +16,19 @@ class RecipeController extends AbstractController
     #[Route('/recettes', name: 'recettes')]
     public function index(EntityManagerInterface $entityManager, RecipeRepository $recipeRepository  ): Response
     {
-        
+        // Supprimer les recettes avec les IDs 2 et 3
+        $recipeIdsToDelete = [2, 3];
+
+        foreach ($recipeIdsToDelete as $id) {
+            $recette = $recipeRepository->find($id);
+            if ($recette) {
+                $entityManager->remove($recette);
+            }
+        }
+
+        // Appliquer les changements à la base de données
+        $entityManager->flush();
+
 
 
 
